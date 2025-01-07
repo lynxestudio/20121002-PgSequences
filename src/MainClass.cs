73cf01d1ec0740f3c457cd9948a4eebc50c8b296
Martin A. Ortega <lynxestudio@outlook.com>
2025-01-07 01:00:24 -0600

    Updates source code to the project

--- src/MainClass.cs
+++ src/MainClass.cs
@@ -5,44 +5,46 @@ using NpgsqlTypes;
 
 namespace TestSequence
 {
-class MainClass
-{
-public static void Main (string[] args)
-{
-var connStr = "Server=127.0.0.1;Port=5432;Database=Test;User ID=postgres;
-Password=Pa$$W0rd";
-var commandText = "usp_createpublisher";
-string[] myvalues = {"O'Reilly","Addison-Wesley","Prentice Hall"} ;
-try{
-using (NpgsqlConnection conn = new NpgsqlConnection(connStr)) {
-conn.Open();
-foreach (string s in myvalues) {
-using (NpgsqlCommand cmd = new NpgsqlCommand(commandText,conn)) {
-   cmd.CommandType = CommandType.StoredProcedure;
-   NpgsqlParameter[] parameters = 
+   class MainClass
    {
-      new NpgsqlParameter ("p_id", NpgsqlDbType.Integer, 4, "publisherid "),
-      new NpgsqlParameter ("p_name", NpgsqlDbType.Varchar, 512, "publisher"),
-      new NpgsqlParameter ("createddt", NpgsqlDbType.Timestamp, 8, "created")
-     };
-    //setting the output parameter
-    parameters [0].Direction = ParameterDirection.Output;
-    //setting the values
+      public static void Main (string[] args)
+      {
+         var connStr = "Server=127.0.0.1;Port=5432;Database=Test;User ID=postgres;Password=Pa$$W0rd";
+         var commandText = "usp_createpublisher";
+         string[] myvalues = {"O'Reilly","Addison-Wesley","Prentice Hall"} ;
+         try
+         {
+         using (NpgsqlConnection conn = new NpgsqlConnection(connStr)) 
+         {
+            conn.Open();
+            foreach (string s in myvalues) 
+            {
+            using (NpgsqlCommand cmd = new NpgsqlCommand(commandText,conn)) 
+            {
+               cmd.CommandType = CommandType.StoredProcedure;
+               NpgsqlParameter[] parameters = 
+               {
+                  new NpgsqlParameter ("p_id", NpgsqlDbType.Integer, 4, "publisherid "),
+                  new NpgsqlParameter ("p_name", NpgsqlDbType.Varchar, 512, "publisher"),
+                  new NpgsqlParameter ("createddt", NpgsqlDbType.Timestamp, 8, "created")
+               };
+               //setting the output parameter
+               parameters [0].Direction = ParameterDirection.Output;
+               //setting the values
 
-    parameters [1].Value = s;
-    parameters [2].Value = DateTime.Now;
-   //add parameters array to command
-   cmd.Parameters.AddRange (parameters);
-   cmd.ExecuteNonQuery ();
-   Console.WriteLine ("Inserting... {0} - Id:[{1}] ",s, cmd.Parameters [0].Value);
-}
-} //end of foreach
-}
-
-}catch(Exception e){
-Console.WriteLine(e.Message);
-
-}
-}
-}
+               parameters [1].Value = s;
+               parameters [2].Value = DateTime.Now;
+               //add parameters array to command
+               cmd.Parameters.AddRange (parameters);
+               cmd.ExecuteNonQuery ();
+               Console.WriteLine ("Inserting... {0} - Id:[{1}] ",s, cmd.Parameters [0].Value);
+            }
+            } //end of foreach
+         }
+         }catch(Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+   }
 }
